4003361f543f7f5cf78fb85d52284ca477bf0c39
Damil9 <Albalupas93949>
2020-12-03 10:55:15 +0100

    fixed code

--- src/labb4/search.java
+++ src/labb4/search.java
@@ -1,40 +1,38 @@
 package labb4;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class search {
 
 	public static void main(String[] args) {
-		if (args.length != 2) {
-			System.out.println("Wrong arguments!");
-			return;
-		}
-
-		String pattern = args[0];
-		String filePath = args[1];
-
-		ArrayList<String> lines = new ArrayList<String>();
-
-		try {
-			Scanner scanner = new Scanner(new File(filePath));
-
-			while (scanner.hasNextLine()) {
-				lines.add(scanner.nextLine());
-			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found, please run the program again!");
-			return;
-		}
-
-		for (String line : lines) {
-			if (line.contains(pattern)) {
-				System.out.println(line);
-			}
-		}
-	}
+        String word = args[0];
+        String path = args[1];
+        
+        try {
+            BufferedReader fReader = new BufferedReader(new FileReader(path));
+            
+            String currentLine;
+            
+            while ((currentLine = fReader.readLine()) != null) {
+                if (currentLine.contains(word)) {
+                    System.out.println(currentLine);
+                }
+            }
+            
+            
+            
+            
+            
+            
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
